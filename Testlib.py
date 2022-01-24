5fdcd8a3eb6bd9df1b1abc296771e49fbc5e3ab2
Alexey <kalashnickov.alexey2015@yandex.ru>
2022-01-24 21:57:43 +0300

    Finding url by searching word

--- Testlib.py
+++ Testlib.py
@@ -1,14 +1,21 @@
+import re
+
+import bs4
+import requests
 import youtube_dl
 from bs4 import BeautifulSoup
+import urllib.request
 
 DL = youtube_dl.YoutubeDL()
 
 
 def start():
     url = input("Link: ")
-    # info = DL.extract_info(url=url, download=False,)
-
-    print(info)
+    html = urllib.request.urlopen(url)
+    video_IDS = re.findall(r"watch\?v=(\S{11})", html.read().decode())
+    print(video_IDS[0], sep="\n")
+    link = "https://www.youtube.com/watch?v=" + video_IDS[0]
+    print(link)
 
 if __name__ == "__main__":
     start()
